c34957f0bb9f26f77eae267fca9fc682b51f26de
m00khin <mookhin@mail.ru>
2024-02-19 08:45:27 +0300

    Commit 19.02.2024 (08:45)

--- music/albums/static/js/modal.js
+++ music/albums/static/js/modal.js
@@ -1,5 +1,5 @@
 (function (){
-    const modal = new bootstrap.Modal(document.getElementById("first"))
+    const modal = new bootstrap.Modal(document.getElementById("modal"))
 
     htmx.on("htmx:afterSwap", (e) => {
         // Response targeting #dialog => show the modal
@@ -23,28 +23,28 @@
 
 })();
 
-(function (){
-    const modal = new bootstrap.Modal(document.getElementById("second"))
-
-    htmx.on("htmx:afterSwap", (e) => {
-        // Response targeting #dialog => show the modal
-        if (e.detail.target.id === "tracks") {
-            modal.show()
-        }
-    })
-
-    htmx.on("htmx:beforeSwap", (e) => {
-        // Empty response targeting #dialog => hide the modal
-        if (e.detail.target.id === "tracks" && !e.detail.xhr.response) {
-            modal.hide()
-            e.detail.shouldSwap = false
-        }
-    })
-
-    htmx.on("hidden.bs.modal", () => {
-        // Remove dialog content after hiding
-        document.getElementById("tracks").innerHTML = ""
-    })
-
-})();
-
+// (function (){
+//     const modal = new bootstrap.Modal(document.getElementById("second"))
+//
+//     htmx.on("htmx:afterSwap", (e) => {
+//         // Response targeting #dialog => show the modal
+//         if (e.detail.target.id === "tracks") {
+//             modal.show()
+//         }
+//     })
+//
+//     htmx.on("htmx:beforeSwap", (e) => {
+//         // Empty response targeting #dialog => hide the modal
+//         if (e.detail.target.id === "tracks" && !e.detail.xhr.response) {
+//             modal.hide()
+//             e.detail.shouldSwap = false
+//         }
+//     })
+//
+//     htmx.on("hidden.bs.modal", () => {
+//         // Remove dialog content after hiding
+//         document.getElementById("tracks").innerHTML = ""
+//     })
+//
+// })();
+//
